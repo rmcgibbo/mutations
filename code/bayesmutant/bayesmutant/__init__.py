b4e9b938c985a63737dc2e564b19aa97378fc70f
Robert McGibbon <rmcgibbo@gmail.com>
2013-08-30 09:31:59 -0700

    Added poster

--- code/bayesmutant/bayesmutant/__init__.py
+++ code/bayesmutant/bayesmutant/__init__.py
@@ -1,4 +1,4 @@
 import version
 from mutant_model import MutantModel
 from simple_sampler import SimpleMutantSampler
-from openmm_sampler import OpenMMMutantSampler
+#from openmm_sampler import OpenMMMutantSampler
